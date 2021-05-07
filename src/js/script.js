ae6f6ae94a19b3ed6e1a05f03454de549a17afac
Nick <sytnyk.m@ukr.net>
2021-05-07 22:22:49 +0300

    second screen

--- src/js/script.js
+++ src/js/script.js
@@ -2,25 +2,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+      closeElem = document.querySelector('.menu__close'),
+      menuLink = document.querySelectorAll('.menu__link'),
+      menuBlock = document.querySelectorAll('.menu__block');
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
-
-function close() {
-	closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
+	hamburger.addEventListener('click', () => {
+	    menu.classList.add('active');
+	    hamburger.classList.add("spanX");
 	});
-}
-close();
-
-document.addEventListener("keydown", (e) => {
-    if (e.code === 'Escape') {
-        close();
-    }
-});
 
 
+	closeElem.addEventListener('click', () => {
+	menu.classList.remove('active');
+	hamburger.classList.remove("spanX");
+	});
 
+	menuLink.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.remove('active');
+            hamburger.classList.remove("spanX");
+        })
+    })
+
+    document.addEventListener("keydown", (e) => {
+	    if (e.code === 'Escape') {
+	        menu.classList.remove('active');
+	        hamburger.classList.remove("spanX");
+	    }
+	});
 });
